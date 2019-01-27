11f8c0524448f690c74f390b9fa712f68cb6785c
DavidSatur <juancawool@outlook.com>
2019-01-27 17:09:12 -0500

    Se agregó el modelo

---
+++ Model/Usuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Usuario
+    {
+
+        private String name;
+        private int age;
+        private bool isBelongs;
+        private String occupation;
+        private int id;
+
+        public Usuario(String name, int age, bool isBelongs, String ocuppation, int id)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.IsBelongs = isBelongs;
+            this.Occupation = ocuppation;
+            this.Id = id;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public bool IsBelongs { get => isBelongs; set => isBelongs = value; }
+        public string Occupation { get => occupation; set => occupation = value; }
+        public int Id { get => id; set => id = value; }
+    }
+}
